9cc2ac3245d3f427165d6e0e7a68685fb6178be1
Shoumen Shomu <shoumenshomu@gmail.com>
2026-02-10 17:24:17 +0600

    add : header a z index added

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -9,7 +9,7 @@ import { MdOutlineFileDownload } from "react-icons/md";
 
 const Header = () => {
   return (
-    <div className="bg-[#181A1D] fixed w-full top-0 left-0">
+    <div className="bg-[#181A1D] fixed w-full top-0 left-0 z-50">
       <Container>
         <div className="py-5">
           <Flex className={"justify-between"}>
