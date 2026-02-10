37970e52860f8715451fb751374961ebcde18b94
Shoumen Shomu <shoumenshomu@gmail.com>
2026-02-10 18:14:19 +0600

    change layouts

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,27 +1,26 @@
-import AboutMe from "../layouts/AboutMe"
-import Banner from "../layouts/Banner"
-import Collaborations from "../layouts/Collaborations"
-import Skills from "../layouts/Skills"
-import Services from "../layouts/Services"
-import Portfolio from "../layouts/Portfolio"
-import Testimonials from "../layouts/Testimonials"
+import AboutMe from "../layouts/AboutMe";
+import Banner from "../layouts/Banner";
+import Collaborations from "../layouts/Collaborations";
+import Skills from "../layouts/Skills";
+import Services from "../layouts/Services";
+import Portfolio from "../layouts/Portfolio";
+import Testimonials from "../layouts/Testimonials";
 
-import ContactPart from "../layouts/ContactPart"
+import ContactPart from "../layouts/ContactPart";
 
 const Home = () => {
   return (
     <div>
-        <Banner/>
-        <Services/>
-        <AboutMe/>
-        <Collaborations/>
-        <Skills/>
-        <Portfolio/>
-        <Testimonials/>
-        <ContactPart/>
-        
+      <Banner />
+      <AboutMe />
+      <Services />
+      <Collaborations />
+      <Skills />
+      <Portfolio />
+      <Testimonials />
+      <ContactPart />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
