76813369598ebeffc3354b1a1fcdda0981241ba1
shoumen-shomu <shoumenshomu@gmail.com>
2026-02-16 00:24:15 +0600

    Header Final Responspive

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({children , className}) => {
     return (
-        <div className={`lg:max-w-292.5 w-full px-4 lg:mx-auto ${className}`}>{children}</div>
+        <div className={`lg:max-w-292.5 w-full px-4 lg:px-0 mx-auto ${className}`}>{children}</div>
     )
 }
 
