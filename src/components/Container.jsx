5451276b622f1411fa9a8f4ddbe345973e3fcda2
shoumen-shomu <shoumenshomu@gmail.com>
2026-02-16 00:07:57 +0600

    width fixed

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({children , className}) => {
     return (
-        <div className={`lg:max-w-292.5 max-w-105 lg:mx-auto ${className}`}>{children}</div>
+        <div className={`lg:max-w-292.5 max-w-97.5 lg:mx-auto ${className}`}>{children}</div>
     )
 }
 
