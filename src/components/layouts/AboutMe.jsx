82e3038cb55a2ede7c4ab9255626078e344def34
shoumen-shomu <shoumenshomu@gmail.com>
2026-02-17 00:01:17 +0600

    About Part  Responsive Done

--- src/components/layouts/AboutMe.jsx
+++ src/components/layouts/AboutMe.jsx
@@ -28,13 +28,13 @@ const AboutMe = () => {
                 brand companies.
               </p>
             </div>
-            <div className="text-left">
+            <div className="text-left mb-5 lg:mb-0">
               <HashLink smooth to="#contact">
                 <Button btnText={"Hire Me"} />
               </HashLink>
             </div>
           </div>
-          <div className="bg-[#212428] lg:p-8 pl-2 rounded-2xl border border-gray-800 lg:w-[45%] transition-all duration-300 shadow-[0_0_20px_rgba(0,0,0,0.1)] overflow-hidden group hover:-translate-y-2">
+          <div className="bg-[#212428] lg:p-8 py-8 lg:py-0 pl-2 lg:pl-0 rounded-2xl border border-gray-800 lg:w-[45%] transition-all duration-300 shadow-[0_0_20px_rgba(0,0,0,0.1)] overflow-hidden group hover:-translate-y-2">
             <h3 className="font-rubik font-medium text-[24px] text-white leading-9 mb-6 border-l-4 border-orange-500 pl-4 group-hover:text-orange-500 transition-colors duration-300">
               Personal Infos
             </h3>
