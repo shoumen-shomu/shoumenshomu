717796cfb8f0516978bae4cc5934e954e634938a
shoumen-shomu <shoumenshomu@gmail.com>
2026-02-19 00:13:32 +0600

    Contact Part Responsive Done

--- src/components/layouts/ContactPart.jsx
+++ src/components/layouts/ContactPart.jsx
@@ -30,39 +30,41 @@ const ContactPart = () => {
 
   return (
     <div id="contact" className="bg-[#212428] border-b border-b-[#181A1D]">
-      <div className="bg-[url(/src/assets/contact.png)] bg-cover bg-center bg-no-repeat pt-25 pb-[30px]">
+      <div className="bg-[url(/src/assets/contact.png)] bg-cover bg-center bg-no-repeat pt-25 pb-7.5">
         <Container>
-          <Flex className={"gap-20"}>
-            <div className="left">
-              <p className="font-rubik font-medium text-[24px] text-[#EE5F38] leading-9">
+          <Flex className={"lg:gap-20 lg:flex-row flex-col"}>
+            <div className="left w-full lg:pw-0">
+              <div className="lg:text-left text-center">
+                <p className="font-rubik font-medium text-[24px] text-[#EE5F38] leading-9 ">
                 CONTACT
               </p>
               <h2 className="font-heebo font-bold text-[48px] text-white leading-14.5">
                 Let’s Start Working Together!
               </h2>
-              <p className="font-rubik font-normal text-[17px] text-[#E2E8F0] leading-7 mt-4 text-justify w-[490px]">
+              <p className="font-rubik font-normal text-[17px] text-[#E2E8F0] leading-7 mt-4 lg:text-justify text-center lg:w-122.5">
                 Ready to launch your project? Reach out to me anytime to discuss
                 your ideas and turn them into reality instantly.
               </p>
-              <div className="mt-[55px]">
+              </div>
+              <div className="mt-13.75">
                 <form ref={form} onSubmit={sendEmail}>
-                  <div className="space-x-[26px]">
+                  <div className="space-x-6.5">
                     <input
-                      className="w-[271px] py-[10px] pl-[15px] bg-transparent border border-[#475569] rounded-lg text-[#b8a9a9] placeholder-[#b8a9a9]"
+                      className="lg:w-67.75 w-[46%] py-2.5 pl-3.75 bg-transparent border border-[#475569] rounded-lg text-[#b8a9a9] placeholder-[#b8a9a9]"
                       type="text"
                       placeholder="Your Name"
                     />
                     <input
-                      className="w-[271px] py-[10px] pl-[15px] bg-transparent border border-[#475569] rounded-lg text-[#b8a9a9] placeholder-[#b8a9a9]"
+                      className="lg:w-67.75 w-[46%] py-2.5 pl-3.75 bg-transparent border border-[#475569] rounded-lg text-[#b8a9a9] placeholder-[#b8a9a9]"
                       type="mail"
                       placeholder="Your Email"
                     />
                   </div>
-                  <div className="mb-[48px]">
+                  <div className="mb-12">
                     <textarea
-                      className="mt-7 py-[10px] pl-[15px] bg-transparent border border-[#475569] rounded-lg text-[#b8a9a9] placeholder-[#b8a9a9]"
+                      className="mt-7 py-2.5 pl-3.75 bg-transparent border border-[#475569] rounded-lg text-[#b8a9a9] placeholder-[#b8a9a9]"
                       rows="5"
-                      cols="73"
+                      cols="51"
                       name=""
                       placeholder="Message"
                     ></textarea>
@@ -71,7 +73,7 @@ const ContactPart = () => {
                 </form>
               </div>
             </div>
-            <div className="right space-y-[50px] pt-[100px]">
+            <div className="right space-y-12.5 pt-25">
               <div className="flex items-center gap-6">
                 <div className="h-17.5 w-17.5 bg-[#EE5F38] rounded-[50%] flex justify-center items-center">
                   <FaPhoneAlt className="text-[25px] text-white" />
@@ -86,7 +88,7 @@ const ContactPart = () => {
                 </div>
               </div>
               <div className="flex items-center gap-6">
-                <div className="h-[70px] w-[70px] bg-[#EE5F38] rounded-[50%] flex justify-center items-center">
+                <div className="h-17.5 w-17.5 bg-[#EE5F38] rounded-[50%] flex justify-center items-center">
                   <MdOutlineEmail className="text-[25px] text-white" />
                 </div>
                 <div className="">
@@ -99,7 +101,7 @@ const ContactPart = () => {
                 </div>
               </div>
               <div className="flex items-center gap-6">
-                <div className="h-[70px] w-[70px] bg-[#EE5F38] rounded-[50%] flex justify-center items-center">
+                <div className="h-17.5 w-17.5 bg-[#EE5F38] rounded-[50%] flex justify-center items-center">
                   <FaMapMarkerAlt className="text-[25px] text-white" />
                 </div>
                 <div className="">
@@ -113,11 +115,11 @@ const ContactPart = () => {
               </div>
             </div>
           </Flex>
-          <div className="mt-[70px] justify-center">
+          <div className="mt-17.5 justify-center">
             <map name="">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d116833.83187894154!2d90.33728812288355!3d23.780975728197344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1770051810485!5m2!1sen!2sbd"
-                width="1220"
+                width="400"
                 height="350"
               ></iframe>
             </map>
