5abfacbd972986bfb21c33579f1685bef92efbb2
shoumen-shomu <shoumenshomu@gmail.com>
2026-02-19 21:14:03 +0600

    map hidden

--- src/components/layouts/ContactPart.jsx
+++ src/components/layouts/ContactPart.jsx
@@ -114,11 +114,11 @@ const ContactPart = () => {
               </div>
             </div>
           </Flex>
-          <div className="mt-17.5 justify-center">
+          <div className="mt-17.5 justify-center hidden lg:block">
             <map name="">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d116833.83187894154!2d90.33728812288355!3d23.780975728197344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1770051810485!5m2!1sen!2sbd"
-                width="400"
+                width="1300"
                 height="350"
               ></iframe>
             </map>
