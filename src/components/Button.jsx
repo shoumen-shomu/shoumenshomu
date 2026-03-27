4a281991182ed484930817eb0c09d3be0d6bfda4
shoumen-shomu <shoumenshomu@gmail.com>
2026-03-28 00:01:04 +0600

    Button Redesign Done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ btnText, className }) => {
   return (
     <button
-      className={`font-rubik font-medium text-[24px] text-[#C1272D] leading-9 py-2.5 px-10.5 rounded-[50px] bg-transparent  border border-[#C1272D] hover:bg-[#C1272D] hover:text-white  duration-500 cursor-pointer ${className}`}
+      className={`font-rubik font-medium text-[24px] text-[#C1272D] leading-9 py-2.5 px-10.5 rounded-[50px] bg-transparent  border border-[#C1272D] hover:bg-[#C1272D] hover:text-[#D1D5DB]  duration-500 cursor-pointer ${className}`}
     >
       {btnText}
     </button>
